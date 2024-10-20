d1f1b11ec91a908eb9f08481a4b500c8fe9a2890
Ayush Goel <ayush0333.be21@chitkara.edu.in>
2024-10-20 13:47:22 +0530

    Created login API

--- src/app.js
+++ src/app.js
@@ -2,19 +2,63 @@ const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
 const User = require('./models/user');
+const {validateSignUpData} = require('./utils/validation');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 
 app.post('/signup', async (req,res) =>{
-    const user = new User(req.body);
     try{
+        // validation of data
+        validateSignUpData(req);
+
+        // Encryption of password
+        const {firstName,lastName,emailId,password} = req.body;
+        const passwordHash = await bcrypt.hash(password,10);
+        console.log(passwordHash);
+
+        // creating new instance of the user model
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
         await user.save();
         res.send("User added successfully");
     }
     catch(err){
-        res.status(400).send("Error saving the user:" + err.message);
+        res.status(400).send("ERROR :" + err.message);
     }
 });
+
+
+app.post('/login', async(req,res) =>{
+    try{
+        const {emailId,password} = req.body;
+
+        if(!validator.isEmail(emailId)){
+            throw new Error("Enter an valid email");
+        }
+
+        const user = await User.findOne({emailId: emailId});
+        if(!user){
+            throw new Error("Invalid Credentials");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if(isPasswordValid){
+            res.send("Login Successfull");
+        }
+        else{
+            throw new Error("Invalid Credentials");
+        }
+    }
+    catch(err){
+        res.status(400).send("ERROR :" + err.message);
+    }
+})
 
 
 app.get('/user', async(req,res) =>{
@@ -58,11 +102,24 @@ app.delete('/user', async(req,res) =>{
 })
 
 
-app.patch('/user', async(req,res) =>{
-    const userId = req.body.userId;
+app.patch('/user/:userId', async(req,res) =>{
+    const userId = req.params?.userId;
     const data = req.body;
 
     try{
+        const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
+
+        const isUpdateAllowed = Object.keys(data).every((k) =>
+            ALLOWED_UPDATES.includes(k)
+        );
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Skills can not be more than 10");
+        }
+
         await User.findByIdAndUpdate({_id: userId}, data, {
             returnDocument: "after",
             runValidators: true,
@@ -81,5 +138,5 @@ connectDB().then(() =>{
         console.log("Server is running on port 3000");
     });
 }).catch(err=>{
-    console.log("Database can not be connected");
+    console.log("Database can not be connected: "+ err);
 });
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -16,10 +17,20 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email Address: " + value);
+            }
+        },
     },
     password: {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password: " + value);
+            }
+        },
     },
     age: {
         type: Number,
@@ -36,6 +47,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://img.freepik.com/premium-vector/businessman-avatar-illustration-cartoon-user-portrait-user-profile-icon_118339-4382.jpg",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid Photo Url: " + value);
+            }
+        },
     },
     about: {
         type: String,
